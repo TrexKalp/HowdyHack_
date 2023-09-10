084f7a07e4e01118e0de146f606695b69532a26c
ValMobile <58125835+ValMobile@users.noreply.github.com>
2023-09-10 10:32:11 -0500

    Add input validations for email addresses and blank registration fields

--- frontend/src/components/SignupCard.jsx
+++ frontend/src/components/SignupCard.jsx
@@ -30,14 +30,25 @@ export default function SignupCard({ switchToLogin, onRegister }) {
   const [lastName, setLastName] = useState("");
 
   const handleSignup = () => {
-    // Store user's credentials in local storage
-    localStorage.setItem(email, password);
-    localStorage.setItem("firstName", firstName);
-    localStorage.setItem("lastName", lastName);
+    const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    alert("Registered Successfully!");
+    if (email.match(emailPattern)) {
+      if (password && firstName) {
+         // Store user's credentials in local storage
+         localStorage.setItem(email, password);
+         localStorage.setItem("firstName", firstName);
+         localStorage.setItem("lastName", lastName);
 
-    switchToLogin();
+         alert("Registered Successfully!");
+      }
+      else {
+          alert("Could not register successfully! Please ensure your first name, password, and email are filled out.");
+      }
+      switchToLogin();
+    }
+    else {
+      alert("Could not register successfully! Please input a valid email!");
+    }
   };
 
   return (
